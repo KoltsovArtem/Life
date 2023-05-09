1409b6f77b12b0fa586c71e78a0f38ac54048cb2
KoltsovArtem <artemklcv@gmail.com>
2023-05-09 15:19:34 +0300

    1

---
+++ Life/src/Game.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace Life
+{
+    public class Game
+    {
+        /*void Update()
+        {
+            if (MainWindow.isEvolving)
+            {
+                InfiniteCoroutine()
+            }
+        }*/
+        public static void Evolve(bool evolving, Grid[] grid)
+        {
+            /*if (MainWindow.isEvolving)
+            {
+                InfiniteCoroutine(grid);
+            }*/
+            //StartCoroutine evolver();
+            Random random = new Random();
+            
+            while (evolving)
+            {
+                if (!MainWindow.isEvolving) break;
+            }
+
+            /*if (MainWindow.isEvolving)
+            {
+                Grid square = grid[25];
+                square.Background = new SolidColorBrush(Color.FromRgb(Convert.ToByte(random.Next(255)),
+                    Convert.ToByte(random.Next(255)), Convert.ToByte(random.Next(255))));
+            }*/
+        }
+
+        static IEnumerator InfiniteCoroutine(Grid[] grid)
+        {
+            Random random = new Random();
+            
+            while (MainWindow.isEvolving)
+            {
+                Grid square = grid[25];
+                square.Background = new SolidColorBrush(Color.FromRgb(Convert.ToByte(random.Next(255)),
+                    Convert.ToByte(random.Next(255)), Convert.ToByte(random.Next(255))));
+            }
+
+            return null;
+        }
+    }
+}
---
+++ Life/src/InitGame.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace Life
+{
+    public class Initializer
+    {
+        public static void unaliveElements(Grid[] grid)
+        {
+            foreach (var element in grid)
+            {
+                element.Tag = "dead";
+                element.Background = new SolidColorBrush(Colors.White);
+            }
+        }
+
+        public static void aliveRandomElements(Grid[] grid)
+        {
+            Random random = new Random();                             //TODO реализовать рандомную генерацию
+        }
+    }
+}
