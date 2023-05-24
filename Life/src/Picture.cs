7d537877a608e8718c4dce648e50b2f019763f9f
Artem <artemklcv@gmail.com>
2023-05-24 15:14:01 +0300

    Заготовка под Пункт №12.

---
+++ Life/src/Picture.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows.Media;
+
+namespace Life
+{
+    public class Picture
+    {
+        public static void Draw(int[,] gen)
+        {
+            /*Bitmap b = new Bitmap(200, 100);
+            using (Graphics g = Graphics.FromImage(b))
+            {
+                g.Clear(Color.White);
+                // что нибудь рисуете
+                // g.DrawRectangle(...
+            }*/
+        }
+    }
+}
