55596b82e29c30cbaf5a805656c316919fdb561c
Artem <artemklcv@gmail.com>
2023-05-25 15:52:01 +0300

    Заготовка под базу данных.

---
+++ Life/Database/Dbclass.cs
@@ -0,0 +1,11 @@
+﻿/*using System.Data.Entity;
+
+public class MyDbContext : DbContext
+{
+    //public DbSet<Person> Persons { get; set; }
+
+    protected override void OnModelCreating(DbModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+    }
+}*/
